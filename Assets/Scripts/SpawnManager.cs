0a9b32a50217538663651ea4c9d5680ed411442a
Chase McCarty <chasemc67@gmail.com>
2020-07-26 17:41:55 -0700

    Character dropping does something, but its hardwired a lot right now

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject activeCharacterPrefab;
+    public Transform rackCharacterTransform;
+    public Transform gameboard;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void spawnCharacter() {
+        Instantiate(activeCharacterPrefab, rackCharacterTransform.position, Quaternion.LookRotation(gameObject.transform.forward, gameObject.transform.up), gameboard);
+    }
+}
